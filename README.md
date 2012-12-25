d9d7178e8e98ec494b0b1160db56f4def9c7e8cd
Sean Nieuwoudt <sean@isean.co.za>
2012-12-25 14:42:57 +0200

    Initial commit + working on the documentation

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
 CI-AssetLoad
 ============
 
-A CodeIgniter library for loading CSS &amp; JS assets from a manifest file according to env's
+A CodeIgniter library for loading CSS & JS assets from a manifest file supporting different environments
+
+Usage:
+-----
+
+1. Download and extract to your CodeIgniter libraries directory
+2. Either autoload the library or explicitly load it in your controllers
+3. Define your asset manifest file for your environments
+4. Replace the css link & script tags with <?php $this->assetload->queue(); ?> in your template head section
+5. Enjoy the sanity that follows! :)
+
+#  Methods
+
+AssetLoad::queue($cache_bust = false, $manifest_path = 'assets/')
+
+# Changing Defaults
+
+The loader assumes all your assets are contained in child directories within the /assets directory in your project root, but you can change this 
+by specifying the parent asset directory as follows:
+
+```php
+<?php $this->assetload->queue(false, '/static'); ?>
+```
+
+If you wish to enable cache-busting by automatically adding a timestamp to include paths, simply change the first argument to the queue() call to true 
+as follows:
+
+```php
+<?php $this->assetload->queue(true); ?>
+```
+
+Defining a Manifest File:
+-----
+
+Your manifest file should be called assets.ini and placed inside your parent assets directory: /assets/assets.ini
---
+++ libraries/AssetLoad.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * AssetLoad - A simple CSS & JS asset loader for CodeIgniter
+ *
+ * @author		Sean Nieuwoudt (http://twitter.com/SeanNieuwoudt)
+ * @copyright	Copyright (c) 2013 wixelhq.com
+ * @link		http://github.com/Wixel/CI-AssetLoad.git
+ * @version     1.0
+ */
+class AssetLoad
+{
+	/**
+	 * Simple queue FIFO mechanism to load the assets
+	 *
+	 * @param boolean $cache_bust
+	 * @param string $manifest_path
+	 * @return void
+	 */
+	public static function queue($cache_bust = false, $manifest_path = 'assets/')
+	{
+		$manifest_file = $manifest_path.'assets.ini';
+		$timestamp 	   = null;
+		
+		if($cache_bust) {
+			$timestamp = '?'.time();
+		}
+		
+		if(!defined('ENVIRONMENT')) {
+			define('ENVIRONMENT', 'development');
+		}
+		
+		if(!file_exists($manifest_file)) {
+			throw new Exception("The asset loader manifest file could not be found at '$manifest_file'");
+		}
+		
+		$manifest = parse_ini_file($manifest_file, true);
+		
+		if(isset($manifest[ENVIRONMENT])) {
+			$css = (isset($manifest[ENVIRONMENT]['css']))? $manifest[ENVIRONMENT]['css'] : array();
+			$js  = (isset($manifest[ENVIRONMENT]['js'])) ? $manifest[ENVIRONMENT]['js']  : array();
+			
+			foreach($css as $e) {
+				echo self::css_link($manifest_path.$e.$timestamp)."\n";
+			}
+			
+			foreach($js as $e) {
+				echo self::script_include($manifest_path.$e.$timestamp)."\n";
+			}	
+			
+			unset($manifest); // clean	
+		}
+	}
+	
+	/**
+	 * Generate the css link tag
+	 *
+	 * @param string $path
+	 * @return string
+	 */
+	private static function css_link($path)
+	{
+		return '<link rel="stylesheet" href="/'.$path.'">';
+	}
+	
+	/**
+	 * Generate the script include tag
+	 *
+	 * @param string $path
+	 * @return string
+	 */
+	private static function script_include($path)
+	{
+		return '<script src="/'.$path.'"></script>';
+	}
+	
+} //EOC
